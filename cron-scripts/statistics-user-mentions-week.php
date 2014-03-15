038ba22eb14513e8f02b23d51a04fe7e0b1db2f3
Laura <laura@scandiweb.com>
2014-03-15 18:07:01 +0200

    Scripts for user mentions statistics update

--- cron-scripts/statistics-user-mentions-week.php
+++ cron-scripts/statistics-user-mentions-week.php
@@ -3,31 +3,32 @@ require(dirname(__FILE__) . '/db-connect.php');
 ini_set('memory_limit', '128M');
 
 /**
- * Update user mentions total count
- * @param date $currentdate
+ * Update user mentions count per week
+ * @param date $currentdate last day of week
  */
-function updateTotalMentionsCount($currentdate) {
+function updateWeekMentionsCount($currentdate) {
     $dbConnect = new database;
     $dbh = $dbConnect->getdbh();
-    $stmt = $dbh->query("SELECT user_id, screen_name, COUNT(occurrences) AS count
+    $stmt = $dbh->query("SELECT user_id, screen_name, DATE(FROM_UNIXTIME(mentioned_at)) AS date, COUNT(occurrences) AS count
                          FROM user_mentions
                          JOIN users ON user_id = id
-                         WHERE DATE(FROM_UNIXTIME(mentioned_at)) < '" . $currentdate . "'
-                         GROUP BY 1");
+                         WHERE DATE(FROM_UNIXTIME(mentioned_at))
+                         BETWEEN '" . date('Y-m-d', strtotime($currentdate . '-1 week + 1 day')) . "' AND '" . $currentdate . "'
+                         GROUP BY 1, 3");
     $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+    var_dump($stmt);
     foreach ($data as $row) {
         $user = $dbh->query("SELECT * FROM statistics_user_mentions WHERE user_id = '" . $row['user_id'] . "'");
         $userData = $user->fetchAll(PDO::FETCH_ASSOC);
         if (!empty($userData)) {
             $dbh->query("UPDATE statistics_user_mentions
-                         SET total_mentions = " . $row['count'] . "
+                         SET last_week_mentions = " . $row['count'] . "
                          WHERE user_id = '" . $row['user_id'] . "'");
         } else {
-            $dbh->query("INSERT INTO statistics_user_mentions (user_id, screen_name, total_mentions)
+            $dbh->query("INSERT INTO statistics_user_mentions (user_id, screen_name, last_week_mentions)
                          VALUES ('" . $row['user_id'] . "', '" . $row['screen_name'] . "', " . $row['count'] . ")");
         }
     }
 }
 
-updateTotalMentionsCount(date('Y-m-d', strtotime(date('Y-m-d') . '-1 day')));
+updateWeekMentionsCount(date('Y-m-d', strtotime(date('Y-m-d') . '-1 day')));
