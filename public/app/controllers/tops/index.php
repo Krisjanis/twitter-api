2bbb0790c2413c9395c0be9e44301957da496974
Laura <laura@scandiweb.com>
2014-03-16 21:39:16 +0200

    Removed hashtags preload in tops page

--- public/app/controllers/tops/index.php
+++ public/app/controllers/tops/index.php
@@ -3,16 +3,9 @@ function _index () {
     $view = new View (APP_PATH . 'views/template.phtml');
     $content = new View (APP_PATH . 'views/tops/index.phtml');
 
-    $content->set('hashtags', _getTopHashatgs(20));
-
     $pageTitle = "Topi";
     $view->set('pageTitle', $pageTitle);
 
     $view->set('content', $content->fetch());
     $view->dump();
-}
-
-function _getTopHashatgs($to) {
-    $hashtags = new Hashtag();
-    return $hashtags->getTopHashtags($to);
 }
