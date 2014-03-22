b66df49f5238ed23b3a377800d56e604d2c7d552
krisjanis.smits <krisjanis.smits@gmail.com>
2014-03-22 23:12:14 +0200

    Removed old framework

--- public/app/controllers/tops/retweets.php
+++
@@ -1,7 +0,0 @@
-<?php
-function _retweets($from = 0, $to = 20) {
-    $retweets = new Statistics_Retweets();
-    $view = new View (APP_PATH . 'views/tops/retweets.phtml');
-    $view->set('retweets', $retweets->getTopRetweetsTotal($from, $to));
-    $view->dump();
-}
