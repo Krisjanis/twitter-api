7df3f1b135c773f1d3bee122eb6cb125d7190a02
aivish <aivis.henins@gmail.com>
2014-04-10 18:53:16 +0300

    Add Google Analytics tracking

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -14,6 +14,17 @@
         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
         <?php echo Asset::js('markerclusterer.js') ?>
         <?php echo Asset::js('uikit.min.js') ?>
+        <?php if ( ! Auth::check()): ?>
+            <script>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+              ga('create', 'UA-49317104-1', 'www.tvito.lv');
+              ga('send', 'pageview');
+            </script>
+        <?php endif; ?>
     </head>
     <body>
         <div class="wrapper <?php echo isset($customClass) ? $customClass : '' ?>">
